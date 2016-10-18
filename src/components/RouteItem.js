67ebd775b60f5b8b9da443c8ee989d9034b75fce
Thomas Dy <thatsmydoing@gmail.com>
2016-10-18 11:31:55 +0800

    Display route type in list

--- src/components/RouteItem.js
+++ src/components/RouteItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+const routeTypes = [
+  'Tram',
+  'Subway',
+  'Rail',
+  'Bus',
+  'Ferry',
+  'Cable car',
+  'Gondola',
+  'Funicular'
+]
+
 export default function RouteItem(props) {
   let name = props.route_short_name || props.route_long_name;
   return (
     <div className="route" title={name}>
       <span className="name">{name}</span>
-      <span className="id">{props.route_id}</span>
+      <span className="id">[{routeTypes[props.route_type]}] {props.route_id}</span>
     </div>
   )
 }
