422b8d23c25a46701836b98edb63440fce72273b
Thomas Dy <thatsmydoing@gmail.com>
2016-10-19 17:54:23 +0800

    Factor out double-line class

--- src/components/RouteItem.js
+++ src/components/RouteItem.js
@@ -5,9 +5,9 @@ import {link} from '../reducers/navigation';
 export default function RouteItem(props) {
   let name = props.route_short_name || props.route_long_name;
   return (
-    <div className="route" title={name}>
-      <span className="name">{name}</span>
-      <span className="id">
+    <div className="double-line" title={name}>
+      <span>{name}</span>
+      <span>
         [{routeTypes[props.route_type]}] {props.route_id} <a href={link('route', props.route_id)}>Details</a>
       </span>
     </div>
--- src/components/TripItem.js
+++ src/components/TripItem.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function TripItem(props) {
   let name = props.trip_headsign || props.trip_short_name || 'Trip '+(props.index+1);
   return (
-    <div className="trip" title={name}>
+    <div className="double-line" title={name}>
       <span>{name}</span>
-      <span className="id">
+      <span>
         {props.trip_id}
       </span>
     </div>
