424a98611e678ea5b19ba9fc6541b52581839da4
Thomas Dy <thatsmydoing@gmail.com>
2016-11-10 21:42:31 +0800

    Style error list

--- src/components/ErrorList.js
+++ src/components/ErrorList.js
@@ -4,8 +4,7 @@ export default function ErrorList(props) {
   let rows = props.feed.errors.map((error, i) => {
     return (
       <tr key={i} className={error.type}>
-        <td>{error.file}</td>
-        <td>{error.line}</td>
+        <td>{error.file}:{error.line}</td>
         <td>{error.message}</td>
       </tr>
     )
@@ -16,9 +15,8 @@ export default function ErrorList(props) {
       <table className="errors">
         <thead>
           <tr>
-            <th>File</th>
-            <th>Line number</th>
-            <th>Error</th>
+            <th className="location">Location</th>
+            <th className="message">Error</th>
           </tr>
         </thead>
         <tbody>
