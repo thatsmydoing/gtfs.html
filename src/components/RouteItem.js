49c7cb4f4b578131bbcfdd6c52abd2d5482afc6b
Thomas Dy <thatsmydoing@gmail.com>
2018-10-20 12:09:42 +0900

    Display unknown route types

--- src/components/RouteItem.js
+++ src/components/RouteItem.js
@@ -4,11 +4,12 @@ import Link from './Link';
 
 export default function RouteItem(props) {
   let name = props.route_short_name || props.route_long_name;
+  let routeType = routeTypes[props.route_type] || `Unknown (${props.route_type})`;
   return (
     <div className="double-line" title={name}>
       <span>{name}</span>
       <span>
-        [{routeTypes[props.route_type]}] {props.route_id} <Link type='route' id={props.route_id}>Details</Link>
+        [{routeType}] {props.route_id} <Link type='route' id={props.route_id}>Details</Link>
       </span>
     </div>
   )
