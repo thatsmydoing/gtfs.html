959e70897be14a22b35f15914fff6afd5c0a5832
Thomas Dy <thatsmydoing@gmail.com>
2020-06-06 22:27:23 +0900

    Handle ids with slashes in navigation

--- src/reducers/navigation.js
+++ src/reducers/navigation.js
@@ -10,7 +10,7 @@ export default function reducer(state = null, action) {
 }
 
 function pathToObject(path) {
-  let [type, id] = decodeURIComponent(path).substring(1).split('/');
+  let [type, id] = decodeURIComponent(path).substring(1).split('/', 2);
   // we force empty string to undefined
   let result = {type: type || undefined, id: id || undefined};
   if(!type && !id) {
